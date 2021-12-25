1239cebf3ec2448ab35bac223b4590a583a976ae
Andriy Khomik <40435970+andriykhomik@users.noreply.github.com>
2021-12-25 18:01:31 +0200

    Finish base framework functional (#3)

---
+++ src/core/DomListener.js
@@ -0,0 +1,42 @@
+import {capitalize} from './utils';
+
+export class DomListener{
+
+    constructor ($root, listeners = []) {
+        if (!$root){
+            throw new Error(`No root provided for DomListener!`);
+        }
+        this.$root = $root;
+        this.listeners = listeners;
+    }
+
+    initDomListeners(){
+
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener);
+            if (!this[method]){
+                const name = this.name;
+                throw new Error(
+                    `Method ${method} is not implemented in ${name} Component`
+                )
+            }
+            this[method] = this[method].bind(this);
+            //the same that 'addEventListener'
+            this.$root.on(listener, this[method]);
+        })
+    }
+
+    removeDomListener(){
+
+        this.listeners.forEach(listener => {
+            console.log (listener);
+            const method = getMethodName(listener);
+            this.$root.off(listener, this[method])
+        })
+    }
+}
+
+// input => onInput
+function getMethodName(eventName){
+    return 'on' + capitalize(eventName);
+}
